c265c4537d9d31b0ee81e07b4e4a814ac2a089cc
Asya404 <unkiplum@gmail.com>
2023-09-02 14:35:44 +0200

    feat: add base card, base button

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="base-button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.base-button {
+  padding: 8px;
+  margin-left: 20px;
+  background-color: transparent;
+  border: 1px solid #eee;
+  color: #fff;
+  border-radius: 5px;
+  width: 35px;
+}
+</style>
--- src/components/expense-items/ExpenseItem.vue
+++ src/components/expense-items/ExpenseItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <div class="expense-item">
+    <base-card class="expense-item">
       <div class="expense-date">
         <div class="expense-date__month">
           {{ expense.date.toLocaleString('en-US', { month: 'long' }) }}
@@ -17,8 +17,8 @@
         <h2>{{ expense.title }}</h2>
         <div class="expense-item__price">{{ expense.price }}</div>
       </div>
-      <button>X</button>
-    </div>
+      <base-button>X</base-button>
+    </base-card>
   </li>
 </template>
 
@@ -36,9 +36,6 @@ export default {
   padding: 0.5rem;
   margin: 1rem 0;
   background-color: #4b4b4b;
-
-  border-radius: 12px;
-  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
 }
 .expense-date {
   display: flex;
@@ -68,28 +65,28 @@ export default {
 }
 
 .expense-item__description {
-    display: flex;
-    gap: 1rem;
-    align-items: flex-end;
-    justify-content: flex-start;
-    flex: 1 1;
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  justify-content: flex-start;
+  flex: 1 1;
 }
 
 .expense-item h2 {
-    color: #3a3a3a;
-    font-size: 1rem;
-    flex: 1 1;
-    margin: 0 1rem;
-    color: #fff;
+  color: #3a3a3a;
+  font-size: 1rem;
+  flex: 1 1;
+  margin: 0 1rem;
+  color: #fff;
 }
 
 .expense-item__price {
-    font-size: 1rem;
-    font-weight: 700;
-    color: #fff;
-    background-color: #40005d;
-    border: 1px solid #fff;
-    padding: 0.5rem;
-    border-radius: 12px;
+  font-size: 1rem;
+  font-weight: 700;
+  color: #fff;
+  background-color: #40005d;
+  border: 1px solid #fff;
+  padding: 0.5rem;
+  border-radius: 12px;
 }
 </style>
